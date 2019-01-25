89f3e5719107454a8b5dd46674d9d0a139d1965e
Christopher Adeszko <cjadeszko@gmail.com>
2019-01-25 12:39:36 -0700

    Create Post and Comment models

---
+++ spec/models/comment_spec.rb
@@ -0,0 +1,13 @@
+require 'rails_helper'
+
+RSpec.describe Comment, type: :model do
+  let(:post) { Post.create!(title: "New Post Title", body: "New post body.") }
+  let(:comment) { Comment.create!(body: "New comment body.", post: post) }
+
+  describe 'attributes' do
+    it 'has a body attribute' do
+      expect(comment).to have_attributes(body: "New comment body.")
+    end
+  end
+  
+end
