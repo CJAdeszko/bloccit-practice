811344685ed4691e66ef523335600facdb71b76f
Christopher Adeszko <cjadeszko@gmail.com>
2019-01-24 18:38:13 -0700

    Create view for FAQ, add route, welcome#faq, and test for new method

--- spec/controllers/welcome_controller_spec.rb
+++ spec/controllers/welcome_controller_spec.rb
@@ -16,4 +16,14 @@ RSpec.describe WelcomeController, type: :controller do
       expect(response).to render_template('about')
     end
   end
+
+  context 'welcome#faq' do
+    describe 'GET faq' do
+      it 'renders the #faq template' do
+        get :faq
+        expect(response).to render_template('faq')
+      end
+    end
+  end
+
 end
