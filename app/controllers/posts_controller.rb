fd263ee3631c9a6bdbef63c1bb2a65cea3b2527f
Christopher Adeszko <cjadeszko@gmail.com>
2019-01-28 13:59:29 -0700

    Create tests for posts#new, posts#create, and posts#show. Implement PostsController for associated actions. Update corresponding views and implement flash messages.

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,13 +1,33 @@
 class PostsController < ApplicationController
   def index
-    @posts = Post.all
+    @posts = Post.all #populates the entire collection of Post objects for the index action
   end
+
 
   def show
+    @post = Post.find(params[:id]) #populates an individual Post object using the :id passed through the params hash
   end
 
+
   def new
+    @post = Post.new
   end
+
+
+  def create
+    @post = Post.new
+    @post.title = params[:post][:title]
+    @post.body = params[:post][:body]
+
+    if @post.save
+      flash[:notice] = "Post saved successfully"
+      redirect_to @post
+    else
+      flash.now[:alert] = "There was an error saving the post. Please try again."
+      render :new
+    end
+  end
+
 
   def edit
   end
