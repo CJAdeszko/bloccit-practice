2a8a69fd266c72a3b9a3d43a87a313a35b52b3b0
Christopher Adeszko <cjadeszko@gmail.com>
2019-01-30 15:34:15 -0700

    Add validations to Post model. Create form partial for Post views. Create error messsage functionality in ApplicationHelper.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,9 @@
 module ApplicationHelper
+
+  def form_group_tag(errors, &block) #Accepts two arguments - an array of errors and a block - '&' turns the block into a proc
+    css_class = 'form_group'
+    css_class << ' has error' if errors.any?
+    content_tag :div, capture(&block), class: css_class #content_tag helper method builds HTML and CSS to display the form element and any errors
+  end
+
 end
--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -9,7 +9,16 @@ RSpec.describe Post, type: :model do
   let(:title) { RandomData.random_sentence }
   let(:body) { RandomData.random_paragraph }
   let(:post) { topic.posts.create!(title: title, body: body) }
+  #Confirm relationship to Topic
   it { is_expected.to belong_to(:topic) }
+  #Validate presence of attributes
+  it { is_expected.to validate_presence_of(:title) }
+  it { is_expected.to validate_presence_of(:body) }
+  it { is_expected.to validate_presence_of(:topic) }
+  #Validate length of attributes
+  it { is_expected.to validate_length_of(:title).is_at_least(5) }
+  it { is_expected.to validate_length_of(:body).is_at_least(20) }
+
 
   describe 'attributes' do
     it 'has title and body attributes' do
